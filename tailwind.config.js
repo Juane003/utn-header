cdb51f3d6ed8928053c8aab2036136b8834dc82d
Juane003 <j.emilio164@gmail.com>
2023-04-13 13:03:35 -0300

    getting started

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "utn-blue": "#223854",
+      },
+    },
+  },
+  plugins: [],
+};
